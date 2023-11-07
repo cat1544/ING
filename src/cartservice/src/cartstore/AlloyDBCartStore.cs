08119c9b86ac8fc2ed38d24f9ea82bb6839177e1
cat1544 <dbstl159@naver.com>
2023-11-07 10:35:33 +0000

    db test (5)

--- src/cartservice/src/cartstore/AlloyDBCartStore.cs
+++ src/cartservice/src/cartstore/AlloyDBCartStore.cs
@@ -62,38 +62,43 @@ namespace cartservice.cartstore
         public async Task AddItemAsync(string userId, string productId, int quantity)
         {
             Console.WriteLine($"AddItemAsync for {userId} called");
-            await using var connection = new NpgsqlConnection(connectionString);
-            await connection.OpenAsync();
-
-            await using var transaction = await connection.BeginTransactionAsync();
             try
             {
+                await using var connection = new NpgsqlConnection(connectionString);
+                await connection.OpenAsync();
+
                 var fetchCmd = $"SELECT quantity FROM {tableName} WHERE userID='{userId}' AND productID='{productId}'";
                 var currentQuantity = 0;
-                await using (var cmdRead = new NpgsqlCommand(fetchCmd, connection, transaction))
+
+                await using (var cmdRead = new NpgsqlCommand(fetchCmd, connection))
                 {
                     await using (var reader = await cmdRead.ExecuteReaderAsync())
                     {
-                        while (await reader.ReadAsync())
-                            currentQuantity += reader.GetInt32(0);
+                        if (await reader.ReadAsync())
+                            currentQuantity = reader.GetInt32(0);
                     }
                 }
+
                 var totalQuantity = quantity + currentQuantity;
 
-                var insertCmd = $"INSERT INTO {tableName} (userId, productId, quantity) VALUES ('{userId}', '{productId}', {totalQuantity})";
-                await using (var cmdInsert = new NpgsqlCommand(insertCmd, connection, transaction))
+                var upsertCmd = $@"
+                    INSERT INTO {tableName} (userId, productId, quantity) 
+                    VALUES ('{userId}', '{productId}', {totalQuantity})
+                    ON CONFLICT (userId, productId) 
+                    DO UPDATE SET quantity = EXCLUDED.quantity;";
+
+                await using (var cmdUpsert = new NpgsqlCommand(upsertCmd, connection))
                 {
-                    await cmdInsert.ExecuteNonQueryAsync();
+                    await cmdUpsert.ExecuteNonQueryAsync();
                 }
-                await transaction.CommitAsync();
             }
             catch (Exception ex)
             {
-                await transaction.RollbackAsync();
                 throw new RpcException(
                     new Status(StatusCode.FailedPrecondition, $"Can't access cart storage at {connectionString}. {ex}"));
             }
         }
+
 
 
 
